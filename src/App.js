919142e277aec02d3fcc12ce304450cd773b0616
Bikash Malu <141820540+Bikash-Malu@users.noreply.github.com>
2024-05-21 15:43:40 +0530

    frontend chcoeckout,login,signup

--- src/App.js
+++ src/App.js
@@ -1,56 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import CardPage from './pages/CardPage';
+import ChecckOut from './pages/CheckOut';
+import SignupPage from './pages/SignupPage';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Home/>
+      ),
+    },
+    {
+      path: "/login",
+      element:<LoginPage/>,
+    },
+    {
+      path: "/signup",
+      element:<SignupPage/>,
+    },
+    {
+      path: "/cart",
+      element:<CardPage/>,
+    },
+    {
+      path: "/checkout",
+      element:<ChecckOut/>,
+    },
+ 
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+  <RouterProvider router={router} />
     </div>
   );
 }
