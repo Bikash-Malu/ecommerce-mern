b1f2daa3ca3c9370bbcaa842fc4358f14aeb0922
Bikash Malu <141820540+Bikash-Malu@users.noreply.github.com>
2024-05-21 16:11:40 +0530

    payemnt option done

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -174,6 +174,7 @@ const ChecckOut = () => {
                     id="push-everything"
                     name="push-notifications"
                     type="radio"
+                    
                     className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                   />
                   <label htmlFor="push-everything" className="block text-sm font-medium leading-6 text-gray-900">
